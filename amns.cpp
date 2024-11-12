73f0e73695a8e5bdb2c72c5f843ebb5339818550
waibiwaibi <2683608563@qq.com>
2024-11-12 14:56:29 +0800

    Initial commit for simple C++ project

--- amns.cpp
+++ amns.cpp
@@ -1,7 +1,6 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	cout<<"hello world";
-	cout << "This is a change in the feature branch!" << endl;
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Git Experiment!" << std::endl;
+    return 0;
 }
